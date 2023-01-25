93e8903bca7b96babf0f8fa702f394f06f769f86
Ricardo Andres Buelvas Cabarcas <123441407+KrabsDevelopers@users.noreply.github.com>
2023-01-25 13:49:33 -0500

    Update Suseciones.py
    I found a more efficient method to be able to evaluate 99% of bouncing numbers within a figure.

--- Suseciones.py
+++ Suseciones.py
@@ -1,32 +1,28 @@
-from itertools import *
-#Función 
-def parejas(iterable):
-    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
-    a, b = tee(iterable)
-    next(b, None)
-    return zip(a, b)
-#Función que devuelve la lista de digitos en una cifra
-def digitos(n):
-    return list(map(int, str(n)))
-#Función que compara o valida si es un numero creciente  
-def num_creciente(n):
-    return all(prev <= curr for prev, curr in parejas(digitos(n)))
-#Función que compara o valida si es un numero decreciente
-def num_decreciente(n):
-    return all(prev >= curr for prev, curr in parejas(digitos(n)))
-#Función que valida si es un número rebotante
-def num_rebote(n):
-    return not num_creciente(n) and not num_decreciente(n)
-#Función que define el total de numeros rebotantes contados 
-def total_ejecutado(iterable):
+def is_bouncy(N):
+    '''Esta función determina si el numero iterado es rebotante.'''
+    number = list(str(N))
+    increasing = list(str(N))
+    decreasing = list(str(N))
+    increasing.sort()
+    decreasing.sort(reverse=True)
+    if (number != increasing and number != decreasing):
+        return True
+    else:
+        return False
+
+
+def find():
+    '''Esta función itera los números de forma ascendente para asi poder evaluar la secuencia de cada cifra. '''
+    start = 1
     total = 0
-    for element in iterable:
-        total += element
-        yield total
-#Función de inicio
-def run():
-    nums = count(1)
-    rebote = total_ejecutado(map(lambda n: float(num_rebote(n)), count(1)))
-    print(next((n for n, r in zip(nums, rebote) if r / n == 0.99)))
-#Codición de ejecución del algoritmo // 
-if __name__ == "__main__": run()
+    bou = 0 
+    while True:
+        total += 1
+        if is_bouncy(start):
+            bou += 1
+        if bou/total >= 0.99:
+            break
+        start += 1
+    return start
+
+print(find())
